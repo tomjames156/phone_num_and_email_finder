448f160c3f3a9ff8a1eb82b54065d5a48e3bd83b
Akinwande Tomisin <tomjames156@gmail.com>
2023-01-27 09:47:11 +0100

    added pseudocode for the program

---
+++ phone_num_email_extractor.py
@@ -0,0 +1,7 @@
+# import pyperclip module
+# store the text from the clipboard in a variable 
+# create a variable for the results
+# create a phone number regex with groups
+# create an email regex with groups
+# join each group together before adding it to the results variable
+# copy the text back to the clipboard
