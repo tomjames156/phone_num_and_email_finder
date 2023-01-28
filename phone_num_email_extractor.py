43c1129c5f5affb95e488874cfba1d890b2f0928
Akinwande Tomisin <tomjames156@gmail.com>
2023-01-28 08:50:04 +0100

    updated the phone nuber regex

--- phone_num_email_extractor.py
+++ phone_num_email_extractor.py
@@ -10,7 +10,7 @@ results_str = ""
 
 # create a phone number regex with groups
 phone_regex = re.compile(r'''
-(\+234|0)?    # country code or starting zero
+(\+243|0)+    # country code or starting zero
 (\s)?    # separator
 (\d{3})    # first three digits 
 (\s)?    # separator
@@ -29,7 +29,6 @@ email_regex = re.compile(r'''
 
 # join each group together before adding it to the results variable
 # copy the text back to the clipboard
-print(phone_regex.search("07068293037").group())
+print(phone_regex.findall("+2437166293027 08155807084 08038672593"))
 emails = email_regex.findall('gbae67@gmail.comtomi134@protonmail.com rosettadiamond@yahoo.com+2437068293037')
-print(emails)
-
+# print(emails)
