ed789ab2668968f65e4eb8d4e8df43b45746dadd
Akinwande Tomisin <tomjames156@gmail.com>
2023-01-28 08:32:40 +0100

    added the email regex

--- phone_num_email_extractor.py
+++ phone_num_email_extractor.py
@@ -20,6 +20,16 @@ phone_regex = re.compile(r'''
 ''', re.VERBOSE)
 
 # create an email regex with groups
+email_regex = re.compile(r'''
+([a-z]+\w+)    # email name
+(@)    # at symbol
+([a-z]+)    # email provider
+(\.com|\.me)    # domain extension
+''', re.VERBOSE)
+
 # join each group together before adding it to the results variable
 # copy the text back to the clipboard
 print(phone_regex.search("07068293037").group())
+emails = email_regex.findall('gbae67@gmail.comtomi134@protonmail.com rosettadiamond@yahoo.com+2437068293037')
+print(emails)
+
