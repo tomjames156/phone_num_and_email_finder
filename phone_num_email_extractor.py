9c80a47844d6193022eab2c3921b874e3dedc673
Akinwande Tomisin <tomjames156@gmail.com>
2023-01-28 09:59:31 +0100

    updated the two regexes and added a function for checking matches

--- phone_num_email_extractor.py
+++ phone_num_email_extractor.py
@@ -3,47 +3,45 @@ import re
 import pyperclip
 
 # store the text from the clipboard in a variable 
-clipboard_text = pyperclip.paste()
+clipboard_text = pyperclip.paste().split()
 
 # create a variable for the results
 results_str = ""
 
 # create a phone number regex with groups
 phone_regex = re.compile(r'''
-(\+243|0)+    # country code or starting zero
+^(\+243|0)+    # country code or starting zero
 (\s)?    # separator
 (\d{3})    # first three digits 
 (\s)?    # separator
 (\d{3})    # second three digits
 (\s)?    # separator
-(\d{4})    # last four digits
+(\d{4})$    # last four digits
 ''', re.VERBOSE)
 
 # create an email regex with groups
 email_regex = re.compile(r'''
-([a-zA-Z0-9]+[_\.-]*[a-zA-Z0-9]+)    # email name
+^([a-zA-Z0-9]+[_\.-]*[a-zA-Z0-9]+)    # email name
 (@)    # at symbol
 ([\w\-]+)    # email provider
 (\.)    # dot separator
-([\w\-\.]+)    # domain extension
+([\w\-\.]+)$    # domain extension
 ''', re.VERBOSE)
 
 # copy the text back to the clipboard
-phone_numbers = phone_regex.findall("+2437166293027 08155807084 08038672593"))
-emails = email_regex.findall('gbae67@gmail.com tomi134@protonmail.com rosettadiamond@yahoo.com +2437068293037 xerox@filly.co.uk noahmonk@gmail.com')
+def is_match(text):
+    """Checks if a text is a phone number or an email"""
+    phone_number = phone_regex.search(text)
+    email = email_regex.search(text)
 
-# join each group together before adding it to the results variable
-def join_group(list_container):
-    """This function joins the parts of a group tuple together"""
+    if((phone_number == None) and (email == None)):
+        return False
+    else:
+        return True
 
-    output = "" # output string
-    
-    for group_tuple in list_container:
-        current_string = ""
-        for element in group_tuple:
-            current_string += element
-        output += f"{current_string} "
-    
-    return output.strip()
+for text in clipboard_text:
+    if(is_match(text)):
+        results_str += f"{text}\n"
 
-print(join_group(emails))
+results_str = results_str.strip()
+print(results_str)
