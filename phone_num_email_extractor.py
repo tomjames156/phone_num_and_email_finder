cd278be35b0c5f54bd797b46dbbcc2f5373e76dc
Akinwande Tomisin <tomjames156@gmail.com>
2023-01-28 08:17:05 +0100

    added some text to the test string file and created the phone number regex

--- phone_num_email_extractor.py
+++ phone_num_email_extractor.py
@@ -1,10 +1,25 @@
-# import pyperclip module
+# import pyperclip module and regex module
+import re
 import pyperclip
 
-print(pyperclip.paste())
 # store the text from the clipboard in a variable 
+clipboard_text = pyperclip.paste()
+
 # create a variable for the results
+results_str = ""
 # create a phone number regex with groups
+
+phone_regex = re.compile(r'''
+(\+234|0)?    # country code or starting zero
+(\s)?    # separator
+(\d{3})    # first three digits 
+(\s)?    # separator
+(\d{3})    # second three digits
+(\s)?    # separator
+(\d{4})    # last four digits
+''', re.VERBOSE)
+
 # create an email regex with groups
 # join each group together before adding it to the results variable
 # copy the text back to the clipboard
+print(phone_regex.search("0706 829 3037").group())
