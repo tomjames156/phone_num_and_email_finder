c9143856ad5108c1727ef80d818d3b66be735b1b
Akinwande Tomisin <tomjames156@gmail.com>
2023-01-29 08:25:46 +0100

    updated thhe programm to use the findall method

--- phone_num_email_extractor.py
+++ phone_num_email_extractor.py
@@ -1,47 +1,43 @@
 # import pyperclip module and regex module
-import re
-import pyperclip
+import re, pyperclip
 
 # store the text from the clipboard in a variable 
-clipboard_text = pyperclip.paste().split()
+clipboard_text = pyperclip.paste()
 
 # create a variable for the results
 results_str = ""
 
 # create a phone number regex with groups
 phone_regex = re.compile(r'''
-^(\+243|0)+    # country code or starting zero
+(\+243|0)+    # country code or starting zero
 (\s)?    # separator
 (\d{3})    # first three digits 
 (\s)?    # separator
 (\d{3})    # second three digits
 (\s)?    # separator
-(\d{4})$    # last four digits
+(\d{4})    # last four digits
 ''', re.VERBOSE)
 
 # create an email regex with groups
 email_regex = re.compile(r'''
-^([a-zA-Z0-9]+[_\.-]*[a-zA-Z0-9]+)    # email name
+([a-zA-Z0-9]+[_\.-]*[a-zA-Z0-9]+)    # email name
 (@)    # at symbol
 ([\w\-]+)    # email provider
 (\.)    # dot separator
-([\w\-\.]+)$    # domain extension
+([\w\-\.]+)    # domain extension
 ''', re.VERBOSE)
 
+# find all email and phone number matches
+emails = email_regex.findall(clipboard_text)
+phone_nos = phone_regex.findall(clipboard_text)
+all_matches = emails + phone_nos
+
+# rejoin each group to a string
+for match_tuple in all_matches:
+    current_match = ""
+    for match_group in match_tuple:
+        current_match += match_group
+    results_str += f"{current_match}\n"
+
 # copy the text back to the clipboard
-def is_match(text):
-    """Checks if a text is a phone number or an email"""
-    phone_number = phone_regex.search(text)
-    email = email_regex.search(text)
-
-    if((phone_number == None) and (email == None)):
-        return False
-    else:
-        return True
-
-for text in clipboard_text:
-    if(is_match(text)):
-        results_str += f"{text}\n"
-
-results_str = results_str.strip()
 pyperclip.copy(results_str)
